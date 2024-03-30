3ac1ff345c58df626137b2fb4d1c270125a36b0b
keseho <yoosharaza29@gmail.com>
2024-03-30 05:57:09 +0530

    userapilimit

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -48,31 +48,6 @@ export const checkApiLimit = async () => {
   }
 };
 
-// export const getApiLimitCount = async () => {
-//   const { userId } = auth();
-
-//   if (!userId) {
-//     return 0;
-//   }
-
-//   const userApiLimit = await prismadb.UserApiLimit.findUnique({
-//     where: {
-//       userId,
-//     },
-//   });
-
-//   if (!userApiLimit) {
-//     return 0;
-//   }
-
-//   return userApiLimit.count;
-// };
-
-//upar wala original hai
-
-//neeche likha code ia to implement 5 queries per day wala hisaab
-const MAX_QUERIES_PER_DAY = 5;
-
 export const getApiLimitCount = async () => {
   const { userId } = auth();
 
@@ -80,27 +55,52 @@ export const getApiLimitCount = async () => {
     return 0;
   }
 
-  // Find user's API limit entry for the current day
-  const userApiLimit = await prismadb.userApiLimit.findFirst({
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
     where: {
       userId,
-      createdAt: {
-        gte: new Date(new Date().setHours(0, 0, 0, 0)), // Start of the day
-        lt: new Date(new Date().setHours(23, 59, 59, 999)), // End of the day
-      },
     },
   });
 
   if (!userApiLimit) {
-    // If no entry exists for the current day, create a new one
-    await prismadb.userApiLimit.create({
-      data: {
-        userId,
-        count: MAX_QUERIES_PER_DAY - 1, // Initial count for the day
-      },
-    });
-    return MAX_QUERIES_PER_DAY - 1;
+    return 0;
   }
 
   return userApiLimit.count;
 };
+
+//upar wala original hai
+
+// //neeche likha code ia to implement 5 queries per day wala hisaab
+// const MAX_QUERIES_PER_DAY = 15;
+
+// export const getApiLimitCount = async () => {
+//   const { userId } = auth();
+
+//   if (!userId) {
+//     return 0;
+//   }
+
+//   // Find user's API limit entry for the current day
+//   const userApiLimit = await prismadb.userApiLimit.findFirst({
+//     where: {
+//       userId,
+//       createdAt: {
+//         gte: new Date(new Date().setHours(0, 0, 0, 0)), // Start of the day
+//         lt: new Date(new Date().setHours(23, 59, 59, 999)), // End of the day
+//       },
+//     },
+//   });
+
+//   if (!userApiLimit) {
+//     // If no entry exists for the current day, create a new one
+//     await prismadb.userApiLimit.create({
+//       data: {
+//         userId,
+//         count: MAX_QUERIES_PER_DAY - 1, // Initial count for the day
+//       },
+//     });
+//     return MAX_QUERIES_PER_DAY - 1;
+//   }
+
+//   return userApiLimit.count;
+// };
